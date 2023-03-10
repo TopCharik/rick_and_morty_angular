5407e8177329b57cb296007b20ceb0c6bd9443cf
Andrii Charkivskyi <q#@>
2023-03-10 23:14:56 +0200

    Handled characters not found error.

--- src/app/characters/characters.service.ts
+++ src/app/characters/characters.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, map, Observable} from "rxjs";
+import {BehaviorSubject, catchError, map, Observable, of, throwError} from "rxjs";
 import {initialPaginationState, PaginationState} from "./models/paginationState";
 import {PaginationInfo} from "./models/paginationInfo";
 import {CharactersQuery} from "./models/characterQuery";
@@ -41,6 +41,10 @@ export class CharactersService {
 
     const httpParams = mapCharacterParamsToHttpParams(charactersParams);
     this.http.get<PaginatedResponse<Character[]>>(this.baseUrl + "/character", {params: httpParams}).pipe(
+      catchError(err => {
+        if (err.error.error === "There is nothing here") return of(emptyApiResponse);
+        return  throwError(() => err)
+      })
     )
       .subscribe({
         next: res => {
@@ -57,4 +61,14 @@ export class CharactersService {
     return this.http.get<Character>(this.baseUrl + `/character/${id}`);
   }
 
+}
+
+const emptyApiResponse:PaginatedResponse<Character[]> = {
+  info: {
+    count: 0,
+    pages: 0,
+    prev: null,
+    next: null,
+  },
+  results: []
 }
