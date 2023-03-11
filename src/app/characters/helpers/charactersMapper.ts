f2e3e6c6e840edd81e50e92198a231d0eedcdcbb
Andrii Charkivskyi <q#@>
2023-03-11 08:48:08 +0200

    mapper fixed.

--- src/app/characters/helpers/charactersMapper.ts
+++ src/app/characters/helpers/charactersMapper.ts
@@ -3,33 +3,34 @@ import {HttpParams} from "@angular/common/http";
 import {Params} from "@angular/router";
 
 export const mapRouteParamsToCharacterQuery = (params: Params): CharactersQuery => {
-  return {
-    name: params['name'] ,
-    type: params['type'] ,
-    status: params['status'] ,
-    species: params['species'] ,
-    gender: params['gender'] ,
-    page: +params['page'],
-  };
+  const page = isNaN(+params['page']) ? 1 : +params['page'];
+  const query: CharactersQuery = {page: page};
+
+  if (params['name']) query.name = params['name'];
+  if (params['type']) query.type = params['type'];
+  if (params['gender']) query.gender = params['gender'];
+  if (params['status']) query.status = params['status'];
+
+  return query;
 }
 export const mapCharacterParamsToHttpParams = (params: CharactersQuery): HttpParams => {
   let httpParams = new HttpParams();
-  if (params.page !== 1){
+  if (params.page !== 1) {
     httpParams = httpParams.append('page', params.page);
   }
-  if (params.name !== undefined){
+  if (params.name !== undefined) {
     httpParams = httpParams.append('name', params.name);
   }
-  if (params.type !== undefined){
+  if (params.type !== undefined) {
     httpParams = httpParams.append('type', params.type);
   }
-  if (params.gender !== undefined){
+  if (params.gender !== undefined) {
     httpParams = httpParams.append('gender', params.gender);
   }
-  if (params.species !== undefined){
+  if (params.species !== undefined) {
     httpParams = httpParams.append('species', params.species);
   }
-  if (params.status !== undefined){
+  if (params.status !== undefined) {
     httpParams = httpParams.append('status', params.status);
   }
   return httpParams;
